0ebce92d59b9200ab3155b4b6970aada9c9bade9
Arely Elias <arelyelias70@gmail.com>
2024-05-23 17:06:11 -0600

    fix: :bug: github actions flow corrections

--- db/db.go
+++ db/db.go
@@ -1,14 +1,34 @@
 package db
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+func getEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
+}
+
 func Init() {
-	dsn := "host=db user=arely password=password dbname=payments port=5432 sslmode=disable"
+	host := getEnv("DB_HOST", "db")
+	user := getEnv("DB_USER", "arely")
+	password := getEnv("DB_PASSWORD", "password")
+	dbname := getEnv("DB_NAME", "payments")
+	port := getEnv("DB_PORT", "5432")
+	sslmode := getEnv("DB_SSLMODE", "disable")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		host, user, password, dbname, port, sslmode)
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
@@ -18,5 +38,7 @@ func Init() {
 }
 
 func ClearDB() {
-	DB.Exec("DELETE FROM merchant")
+	DB.Exec("DELETE FROM merchants")
+
+	DB.Exec("DELETE FROM transactions")
 }
--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"arely.dev/db"
 	"arely.dev/models"
 	"arely.dev/server"
 )
 
-func main() {
-	db.Init()
-	err := db.DB.AutoMigrate(&models.Merchant{})
+func runMigrations() {
+	err := db.DB.AutoMigrate(&models.Merchant{}, &models.Transaction{})
 	if err != nil {
 		panic(err)
 	}
-	err = db.DB.AutoMigrate(&models.Transaction{})
-	if err != nil {
-		panic(err)
+}
+
+func main() {
+	db.Init()
+	runMigrations()
+
+	migrateFlag := flag.Bool("migrate", false, "Run database migrations")
+	flag.Parse()
+
+	if *migrateFlag {
+		fmt.Println("Migrations executed successfully.")
+		return
 	}
 
 	server.Init()
