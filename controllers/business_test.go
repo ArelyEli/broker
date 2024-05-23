a9a3d10993a37dc1f893a243f3693d58b644a900
Arely Elias <arelyelias70@gmail.com>
2024-05-23 13:01:38 -0600

    test: :white_check_mark: Testing added

--- controllers/business_test.go
+++ controllers/business_test.go
@@ -3,9 +3,11 @@ package controllers
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -25,7 +27,7 @@ func TestMain(m *testing.M) {
 	os.Exit(exitVal)
 }
 
-func TestMerchantController_CreateBusiness(t *testing.T) {
+func TestCreateBusinessController(t *testing.T) {
 	db.ClearDB()
 
 	w := httptest.NewRecorder()
@@ -51,28 +53,100 @@ func TestMerchantController_CreateBusiness(t *testing.T) {
 	assert.Equal(t, requestBody.Commission, createdBusiness.Commission)
 }
 
-// func TestBusinessController_BadRequestCreateBusiness(t *testing.T) {
-// 	db.ClearDB()
+func TestCreateBusinessNoValidValueController(t *testing.T) {
+	db.ClearDB()
 
-// 	w := httptest.NewRecorder()
-// 	c, _ := gin.CreateTestContext(w)
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
 
-// 	requestBody := schemas.BusinessCreateRequest{
-// 		Name:       "Test Business",
-// 		Commission: 10.0,
-// 	}
-// 	requestBodyBytes, _ := json.Marshal(requestBody)
+	requestBody := schemas.CreateBusinessRequest{
+		Name:       "Test Business",
+		Commission: 1000,
+	}
+	requestBodyBytes, _ := json.Marshal(requestBody)
 
-// 	req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(requestBodyBytes))
-// 	req.Header.Set("Content-Type", "application/json")
-// 	c.Request = req
+	req, _ := http.NewRequest("POST", "/", bytes.NewBuffer(requestBodyBytes))
+	req.Header.Set("Content-Type", "application/json")
+	c.Request = req
 
-// 	CreateBusinessController(c)
+	CreateBusinessController(c)
 
-// 	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
 
-// 	var createdBusiness models.Business
-// 	db.DB.First(&createdBusiness, "name = ?", "Test Business")
-// 	assert.Equal(t, requestBody.Name, createdBusiness.Name)
-// 	assert.Equal(t, createdBusiness.Commission, 10.0)
-// }
+func TestUpdateBusinessController(t *testing.T) {
+	db.ClearDB()
+
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	business := models.Business{
+		Name:       "Test Business",
+		Commission: 10.0,
+	}
+	business.CreateBusiness()
+
+	requestBody := schemas.UpdateBusinessRequest{
+		Name:       "Updated Business",
+		Commission: 20.0,
+	}
+	requestBodyBytes, _ := json.Marshal(requestBody)
+
+	req, _ := http.NewRequest("PATCH", "/", bytes.NewBuffer(requestBodyBytes))
+	req.Header.Set("Content-Type", "application/json")
+	c.Request = req
+	c.Params = append(c.Params, gin.Param{Key: "id", Value: fmt.Sprintf("%d", business.ID)})
+
+	UpdateBusinessController(c)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	var updatedBusiness models.Business
+	updatedBusiness.GetBusiness(strconv.Itoa(int(business.ID)))
+	assert.Equal(t, requestBody.Name, updatedBusiness.Name)
+	assert.Equal(t, requestBody.Commission, updatedBusiness.Commission)
+}
+
+func TestGetEarningsByBusinessController(t *testing.T) {
+	db.ClearDB()
+
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	business := models.Business{
+		Name:       "Test Business",
+		Commission: 10.0,
+	}
+	business.CreateBusiness()
+
+	first_transaction := models.Transaction{
+		Amount:     123.0,
+		Fee:        123.0 * business.Commission / 100,
+		BusinessID: business.ID,
+	}
+	first_transaction.CreateTransaction()
+
+	second_transaction := models.Transaction{
+		Amount:     234.0,
+		Fee:        234.0 * business.Commission / 100,
+		BusinessID: business.ID,
+	}
+	second_transaction.CreateTransaction()
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	c.Request = req
+	c.Params = append(c.Params, gin.Param{Key: "id", Value: fmt.Sprintf("%d", business.ID)})
+
+	GetEarningsByBusinessController(c)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	type testResponse struct {
+		Earnings float64 `json:"earnings"`
+	}
+
+	var response testResponse
+	json.Unmarshal(w.Body.Bytes(), &response)
+
+	assert.Equal(t, first_transaction.Fee+second_transaction.Fee, response.Earnings)
+}
